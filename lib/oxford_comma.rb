ccd0eb80f26e25be4607feda202135408733bf3f
Randi Schreiner <mimirandi21@gmail.com>
2020-09-29 14:48:10 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'rails'
 
+def oxford_comma(array)
+    if array.length <= 1
+        return array.join
+    elsif array.length == 2
+        return array[0] + " and " + array[1]
+    else
+        string_array = array.to_sentence
+        
+    end
+    return string_array    
 end
